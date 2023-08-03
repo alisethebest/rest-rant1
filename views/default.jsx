06e803355702363c417135b47d26dd7bf7d3ec44
Alise Cervantes <alise09@icloud.com>
2023-08-02 23:36:40 -0700

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,11 +5,11 @@ function Def(html) {
     <html>
       <head>
         <title>Title</title>
+        <link rel="stylesheet" href="/css/style.css" />
       </head>
       <body>{html.children}</body>
     </html>
   );
 }
-
 
 module.exports = Def;
