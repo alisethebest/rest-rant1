a62f357d72271753afc47170a61bd13d114ff2cf
Alise Cervantes <alise09@icloud.com>
2023-08-03 09:11:52 -0700

    rest-rant

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,28 @@
 const places = require("express").Router();
-
+const places = require("../models/places.js");
 places.get("/", (req, res) => {
-    const places = [
-      {
-        name: "H-Thai-ML",
-        city: "Seattle",
-        state: "WA",
-        cuisines: "Thai, Pan-Asian",
-        pic: "/images/chick-fil-a.png",
-      },
-      {
-        name: "Coding Cat Cafe",
-        city: "Phoenix",
-        state: "AZ",
-        cuisines: "Coffee, Bakery",
-        pic: "/images/in-n-out.png",
-      },
-    ];
   res.render("places/Index", { places });
 });
 
-places.get('/new', (req, res) => {
-res.render('places/New')
+places.get("/new", (req, res) => {
+  res.render("places/New");
 });
 
-places.post('/', (req, res) => {
+places.post("/", (req, res) => {
   console.log(req.body);
+  if (!req.body.pic) {
+    // Default image i one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
+
   res.send(200);
 });
 
